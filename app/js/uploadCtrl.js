27e31abc8364ad1a03dec77767a648d35ccde92e
Andreas Gerlach <andi@appelgriebsch.com>
2015-09-15 13:09:34 +0200

    initial upload documents

--- app/js/uploadCtrl.js
+++ app/js/uploadCtrl.js
@@ -7,13 +7,45 @@
   function UploadController($state, $log, $q) {
 
     var self = this;
+    var fs = require('fs');
+
+    self.files = [];
 
     self.initialize = function() {
 
+      var dropZone = document.querySelector('#dropZone');
+
+      dropZone.ondragover = function(e) {
+        e.dataTransfer.dropEffect = 'copy';
+        return false;
+      };
+
+      dropZone.ondragleave = dropZone.ondragend = function() {
+        return false;
+      };
+
+      dropZone.ondrop = function(e) {
+        e.preventDefault();
+        var files = e.dataTransfer.files;
+        for (var i = 0; i < files.length; ++i) {
+          var file = files[i];
+          var fInfo = fs.statSync(file.path);
+          self.files.push({
+            type: fInfo.isDirectory() ? "folder" : "file",
+            mime: file.type,
+            size: file.size,
+            name: file.name,
+            path: file.path,
+            uploadProgress: 0
+          });
+        };
+        return false;
+      };
     };
 
-    self.addItem = function() {};
-
+    self.submit = function() {
+      console.log(self.files);
+    };
   };
 
 })();
