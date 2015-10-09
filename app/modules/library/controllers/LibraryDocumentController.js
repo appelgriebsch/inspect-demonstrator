207c1fdd160d2bf3791475bb5018c65b34ee302e
Andreas Gerlach <andreas.gerlach@smail.fh-koeln.de>
2015-10-09 06:37:18 +0200

    canvas, scrolling & mouse wheel handling

--- app/modules/library/controllers/LibraryDocumentController.js
+++ app/modules/library/controllers/LibraryDocumentController.js
@@ -6,22 +6,66 @@
 
     var nativeImage = require('native-image');
     var docID = $stateParams.doc;
+    var imageData;
 
     this.document;
     this.action;
     this.sidebarOpened = false;
+    this.scaleFactor = 1.0;
+    this.mousePos = {
+      x: 0,
+      y: 0,
+      deltaX: 0,
+      deltaY: 0
+    };
+
+    var render = () => {
+
+      var canvas = document.getElementById('page');
+      var ctx = canvas.getContext('2d');
+
+      canvas.width = canvas.parentNode.clientWidth;
+      canvas.height = canvas.parentNode.clientHeight;
+
+      var cw = canvas.width;
+      var imgW = imageData.width;
+      var ch = canvas.height;
+      var imgH = imageData.height;
+
+      var scaleX = cw / imgW;
+      var scaleY = ch / imgH;
+
+      console.log(this.scaleFactor, this.mousePos);
+
+      ctx.clearRect(0, 0, cw, ch);
+      ctx.save();
+      ctx.scale(scaleX * this.scaleFactor, scaleX * this.scaleFactor);
+      ctx.translate(Math.min(this.mousePos.deltaX, cw), Math.min(this.mousePos.deltaY, ch));
+      ctx.drawImage(imageData, -1 * this.mousePos.x, -1 * this.mousePos.y);
+      ctx.restore();
+    }
 
     this.initialize = function() {
+
+      document.body.onresize = render;
+      document.getElementById('page').addEventListener('mousewheel', (evt) => {
+        this.mousePos.deltaX += evt.wheelDeltaX;
+        this.mousePos.deltaY += evt.wheelDeltaY;
+        render();
+      });
+
       $q.when(LibraryDataService.initialize())
         .then(() => {
           $q.when(LibraryDataService.item(docID))
-          .then((result) => {
-            if (result._attachments) {
-              result.image = nativeImage.createFromBuffer(result._attachments[result.canonicalID].data);
-            }
-            result.custom_tags = [];
-            this.document = result;
-          });
+            .then((result) => {
+              if (result._attachments) {
+                imageData = new Image();
+                imageData.onload = render;
+                imageData.src = nativeImage.createFromBuffer(result._attachments[result.canonicalID].data).toDataUrl();
+              }
+              result.custom_tags = [];
+              this.document = result;
+            });
         });
     };
 
@@ -39,46 +83,58 @@
       });
     };
 
-    this.runAction = function(evt) {
-      console.log(evt);
-      switch(this.action) {
+    this.runAction = (evt) => {
+
+      evt.preventDefault();
+      evt.stopPropagation();
+
+      var rect = evt.target.getBoundingClientRect();
+
+      this.mousePos.x = parseInt(evt.clientX - rect.left);
+      this.mousePos.y = parseInt(evt.clientY - rect.top);
+
+      switch (this.action) {
         case "zoom-in":
-          angular.element(document.querySelector('.pagePreview')).removeClass('zoom-in-activated');
+          angular.element(document.querySelector('#page')).removeClass('zoom-in-activated');
+          this.scaleFactor += 0.1;
+          render();
           break;
 
         case "zoom-out":
-          angular.element(document.querySelector('.pagePreview')).removeClass('zoom-out-activated');
+          angular.element(document.querySelector('#page')).removeClass('zoom-out-activated');
+          this.scaleFactor -= 0.1;
+          render();
           break;
 
-          case "annotate":
-            angular.element(document.querySelector('.pagePreview')).removeClass('annotate-activated');
-            break;
+        case "annotate":
+          angular.element(document.querySelector('#page')).removeClass('annotate-activated');
+          break;
       }
     };
 
     $scope.$on('zoom-in', (event, args) => {
-      angular.element(document.querySelector('.pagePreview')).addClass('zoom-in-activated');
+      angular.element(document.querySelector('#page')).addClass('zoom-in-activated');
       this.action = "zoom-in";
     });
 
     $scope.$on('zoom-out', (event, args) => {
-      angular.element(document.querySelector('.pagePreview')).addClass('zoom-out-activated');
+      angular.element(document.querySelector('#page')).addClass('zoom-out-activated');
       this.action = "zoom-out";
     });
 
     $scope.$on('annotate', (events, args) => {
-      angular.element(document.querySelector('.pagePreview')).addClass('annotate-activated');
+      angular.element(document.querySelector('#page')).addClass('annotate-activated');
       this.action = "annotate";
     });
 
     $scope.$on('remove-document', (event, args) => {
 
       var confirm = $mdDialog.confirm()
-                      .title('Would you like to delete this document?')
-                      .content(this.document.title)
-                      .targetEvent(args)
-                      .ok('Yes, delete it')
-                      .cancel('No, please keep it');
+        .title('Would you like to delete this document?')
+        .content(this.document.title)
+        .targetEvent(args)
+        .ok('Yes, delete it')
+        .cancel('No, please keep it');
       $mdDialog.show(confirm).then(() => {
         LibraryDataService.delete(this.document).then(() => {
 
@@ -92,7 +148,7 @@
             id: details._id,
             details: details
           };
-          ActivityService.addWarning(info).then(() =>{
+          ActivityService.addWarning(info).then(() => {
             $state.go('^.view');
           });
         });
