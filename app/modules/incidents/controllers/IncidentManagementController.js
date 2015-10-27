a87d651cdbaa663c56ee99840bfdbcd561dd8a4e
Andreas Gerlach <andreas.gerlach@smail.fh-koeln.de>
2015-10-27 16:57:48 +0100

    controller for form values

--- app/modules/incidents/controllers/IncidentManagementController.js
+++ app/modules/incidents/controllers/IncidentManagementController.js
@@ -7,6 +7,7 @@
     var docID = $stateParams.doc;
     var terminalTypes = ['NFC', 'Contact'];
     var usageTypes = ['EMV', 'Magnetic Stripe'];
+    var verificationTypes = ['PIN (plain)', 'PIN (encr.)', 'Signature', 'No CVM'];
 
     this.document = null;
     this.connectivity = false;
@@ -16,16 +17,10 @@
       Promise.all(init).then(() => {
         if (docID) {
           return IncidentDataService.get(docID);
-        } else {
-          return {
-            terminal: '',
-            connectivity: 'offline',
-            usage: ''
-          };
         }
       }).then((result) => {
         console.log(result);
-        this.document = result;
+        this.document = result.rows[0].doc;
       }).catch((err) => {
         $scope.setError(err);
       });
@@ -39,11 +34,29 @@
       return usageTypes;
     };
 
+    this.verificationTypes = function() {
+      return verificationTypes;
+    };
+
     this.toggleConnectivity = () => {
       $q.when(true).then(() => {
         this.document.connectivity = (this.connectivity ? 'online' : 'offline');
       });
     };
+
+    $scope.$on('submit', (event, args) => {
+      
+      $scope.setBusy('Saving incident data...');
+
+    });
+
+    $scope.$on('cancel', (event, args) => {
+      $q.when(true).then(() => {
+        this.document = null;
+        $scope.setReady(false);
+        $state.go('^.view');
+      });
+    });
   }
 
   module.exports = IncidentManagementController;
