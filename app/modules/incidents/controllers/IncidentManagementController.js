76a59fc6b8c1c8b4751e8cd08bffb19709cadcda
Andreas Gerlach <andreas.gerlach@smail.fh-koeln.de>
2015-10-27 12:59:17 +0100

    terminal type selector

--- app/modules/incidents/controllers/IncidentManagementController.js
+++ app/modules/incidents/controllers/IncidentManagementController.js
@@ -5,23 +5,29 @@
   function IncidentManagementController($scope, $state, $stateParams, $q, IncidentDataService) {
 
     var docID = $stateParams.doc;
+    var terminalTypes = ['NFC', 'Contact'];
 
     this.document = null;
 
     this.initialize = function() {
       var init = [IncidentDataService.initialize()];
       Promise.all(init).then(() => {
-        console.log(docID);
         if (docID) {
           return IncidentDataService.get(docID);
         } else {
-          return $q.when(true);
+          return {
+            terminal: ''
+          };
         }
       }).then((result) => {
         console.log(result);
       }).catch((err) => {
         $scope.setError(err);
       });
+    };
+
+    this.terminalTypes = function() {
+      return terminalTypes;
     };
   }
 
