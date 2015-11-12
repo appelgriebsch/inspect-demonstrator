f49123f65ade96a496848719ddd5cb65213f7d60
Andreas Gerlach <andi@appelgriebsch.com>
2015-11-12 16:58:05 +0100

    initial store meta info for web sites into database

--- app/modules/library/controllers/LibraryCaptureController.js
+++ app/modules/library/controllers/LibraryCaptureController.js
@@ -16,7 +16,8 @@
 
       DocumentCaptureService.captureWebSite(this.url).then((result) => {
 
-        this.document.preview = result.preview;
+        this.document.meta.thumbnailUrl.encodingFormat = result.content_type;
+        this.document.meta.thumbnailUrl.contentUrl = result.preview;
 
         var _attachments = this.document._attachments || {};
         _attachments[result.id] = {
@@ -29,7 +30,7 @@
 
           var info = $scope.createEventFromTemplate('AddAction', 'public');
           info.description = `Web Site <i>${result.meta.name}</i> captured successfully!`;
-          info.object = this.document;
+          info.object = this.document.meta;
           delete info.result;
 
           $scope.writeLog('info', info).then(() => {
@@ -74,13 +75,19 @@
       var template = LibraryDataService.createMetadataFromTemplate('website');
       template.author = LibraryDataService.createMetadataFromTemplate('person');
 
+      delete template.author.description;
+      delete template.author.email;
+      delete template.author.honorificPrefix;
+      delete template.author.honorificSuffix;
+      delete template.author.jobTitle;
+
       var author = meta.author.split(/\s*,\s*/);
       if (author.length > 1) {
         template.author.familyName = author[0];
         template.author.givenName = author[1];
         template.author.name = `${author[1]} ${author[0]}`;
       } else {
-        author = meta.author.split(" ");
+        author = meta.author.split(' ');
         template.author.name = meta.author;
         if (author.length > 0) {
           template.author.familyName = author[1];
@@ -99,7 +106,7 @@
       template.url = meta.url;
 
       console.log(template);
-      
+
       $q.when(true).then(() => {
         this.document = {
           meta: template,
