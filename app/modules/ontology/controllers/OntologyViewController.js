8ac70a36cf46d78d2b8520e0a469eac3c91b7721
Andreas Gerlach <andi@appelgriebsch.org>
2016-05-10 12:30:21 +0200

    initial graph visualization of Skimmer class

--- app/modules/ontology/controllers/OntologyViewController.js
+++ app/modules/ontology/controllers/OntologyViewController.js
@@ -4,6 +4,8 @@
 
   function OntologyViewController($scope, $state, $q, $mdDialog, OntologyDataService) {
 
+    this.nodes = [];
+    this.edges = [];
     this.state = $state.$current;
     this.baseState = this.state.parent.toString();
 
@@ -14,7 +16,32 @@
         return OntologyDataService.node('http://www.AMSL/GDK/ontologie#Skimmer');
       }).then((results) => {
         $q.when(true).then(() => {
-          console.log(results);
+          var i = 0;
+          results.map((item) => {
+            var subjNode = {
+              id: ++i,
+              label: item.subject
+            };
+            var objNode = {
+              id: ++i,
+              label: item.object
+            };
+            this.nodes.push(subjNode);
+            this.nodes.push(objNode);
+            this.edges.push({
+              from: subjNode.id,
+              to: objNode.id,
+              label: item.predicate
+            });
+
+            var container = document.getElementById('ontology-graph');
+            var data = {
+              nodes: this.nodes,
+              edges: this.edges
+            };
+            var options = {};
+            var network = new vis.Network(container, data, options);
+          });
         });
       });
     };
