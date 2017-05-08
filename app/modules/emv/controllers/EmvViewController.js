86a83c82389ed9ccab71090f6be59e0d9da2f745
esspricht <michael@berrezak.com>
2017-05-08 10:08:16 +0200

    changed EMV design, refreshed screenshots and added EMV log

--- app/modules/emv/controllers/EmvViewController.js
+++ app/modules/emv/controllers/EmvViewController.js
@@ -5,6 +5,7 @@
     function EmvViewController($scope, $state, $q) {
         var path = require('path');
         this.events = [];
+        this.emvLogScreen = path.join(__dirname, '../assets', 'emvlog.png');
         this.emvStartScreen = path.join(__dirname, '../assets', 'slice1.png');
         this.emvTime1 = path.join(__dirname, '../assets', 'slice2.png');
         this.emvTime2 = path.join(__dirname, '../assets', 'slice3.png');
